99feba6f1494d5f6d5e86824ff5cf640ce70acba
Grp-25SkinLesion <98631391+Grp-25SkinLesion@users.noreply.github.com>
2022-02-28 15:48:29 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -65,13 +65,13 @@ async def analyze(request):
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
-@app.route('/severity', methods=['POST'])
-async def severity(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn1.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+# @app.route('/severity', methods=['POST'])
+# async def severity(request):
+#     img_data = await request.form()
+#     img_bytes = await (img_data['file'].read())
+#     img = open_image(BytesIO(img_bytes))
+#     prediction = learn1.predict(img)[0]
+#     return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
